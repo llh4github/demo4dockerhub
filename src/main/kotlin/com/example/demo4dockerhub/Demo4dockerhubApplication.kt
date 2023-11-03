e588496c47311fb016acf3224a28eff4072f668d
llh <linhongli@lilith.com>
2023-11-03 15:06:45 +0800

    init

---
+++ src/main/kotlin/com/example/demo4dockerhub/Demo4dockerhubApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo4dockerhub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Demo4dockerhubApplication
+
+fun main(args: Array<String>) {
+    runApplication<Demo4dockerhubApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demo4dockerhub/Demo4dockerhubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demo4dockerhub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Demo4dockerhubApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
